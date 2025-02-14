75d2292231d00e4835e7e79f11d3a113e89321e5
MICHAELROHIN <michaelrohin@gmail.com>
2025-02-14 07:45:16 -0800

    created

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '100vh',
+    fontSize: '100px', 
+    color: 'WHITE',
+};
+
+return (
+  <div style={containerStyle}>
+      <p>
+          MICHAEL ROHIN
+      </p>
+  </div>
+  )
+}
+
+export default App
