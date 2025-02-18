09522c422d7443ea428a1f59b979a1b9c2afe201
MICHAELROHIN <michaelrohin@gmail.com>
2025-02-18 08:04:51 -0800

    doubleclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,55 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css' 
+import Comp1 from './Comp1.jsx'
 
 function App() { 
-  let name = "Michael Rohin"
-  // => arrow function
-  
-  const mydetails={  //const is the keyword
-    name: "Hi, Hello Vanakkam",
-    age: 18,
-    dept: "M.Tech Computer Science And Engineering",
-  }
-  
-  alert(`${mydetails.name}`)
-  console.log(`Age is ${mydetails.age}`)
-const michael=()=>{
-  console.log("Our department is best");
-}
+  const myInfo = {
+    name1: "Michael Rohin",
+    age1: 20,
+    secId: "SEC23CJ029",
+    department: "M.Tech CSE"
+  };
 
-  return ( 
-    
+  const himInfo = {
+    name2: "MUGILAN",
+    age2: 20,
+    secId2: "SEC23CJ042",
+    department2: "M.Tech CSE"
+  };
+
+  return (
     <>
-      <h1 style={{textAlign:"center"}}>{mydetails.dept}</h1>  {/*inline style*/} 
-      
-      {/* <h1 style={rStyle}>Rohin</h1> 
-       internal style  */}
-      {/* <h1>{mydetails.name}</h1> */}
-      {/* <p>my name is {name}</p> */}
-      <p className='container'>Name: {mydetails.name}<br/>  {/* external css */}
-      
-        Age: {mydetails.age}<br/>
-        Department: {mydetails.dept}<br/>
-      </p>
-    <button onClick={App}>Please click me</button>
-     <button onClick={michael}>hi, click me</button>
-     <button onClick={()=>console.log("Naan dhan da Leo")}>yellarukkum vanakkom</button>
+      <div 
+        className='container'
+        style={{
+          backgroundImage: "url('https://miro.medium.com/v2/resize:fit:1400/0*9I-5BaHDOZRMdgD9.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          minHeight: "100vh",
+          padding: "20px"
+        }}
+      >
+        <h1 style={{ textAlign: "center", color: "darkred" }}>Friends Forever</h1>
+        <br />
+
+        <div>
+          <h1 style={{ fontSize: "30px", textAlign: "center", color: "white", fontStyle: "italic" }}>Me</h1>
+          <p style={{ fontSize: "30px", textAlign: "center", color: "white", fontStyle: "italic"}}>
+          <Comp1 name={myInfo.name1} age={myInfo.age1} secId={myInfo.secId} department={myInfo.department} />
+          </p>
+          
+          <h1 style={{ fontSize: "30px", textAlign: "center", color: "White", fontStyle: "italic" }}>My Friend</h1>
+          <p style={{ fontSize: "30px", textAlign: "center", color: "white", fontStyle: "italic"}}>
+          <Comp1 name={himInfo.name2} age={himInfo.age2} secId={himInfo.secId2} department={himInfo.department2} />
+          </p>
+        </div>
+        
+        <button onClick={() => alert("Vanakkam!")}>Click</button>
+        <button onDoubleClick={() => alert("I am the king of the Indian Ocean!")}>Double Click Me</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
